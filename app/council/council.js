587d418ac736a243c10ed606f1603ed128ff8c6c
Jason Neylon <jason.neylon@gmail.com>
2014-09-13 19:43:31 +0100

    Update messaging

--- app/council/council.js
+++ app/council/council.js
@@ -13,17 +13,16 @@ angular.module('myApp.council', ['ngRoute'])
   function($scope, $routeParams, $http, Region) {
     Region.query({name: $routeParams.region}, function(region) {
       $scope.council = _(region).find({ LA_id: $routeParams.laId });
+      $scope.council_count = region.length;
+      $scope.best_council = _(region).min("rank").value();
+      console.log($scope.best_council);
     });
 
-    $scope.region = $routeParams.region
-
+    $scope.regionName = $scope.region = $routeParams.region
 
    $http.get("data/maps/2490.geojson").success(function(data, status) {
       var point = data.coordinates[0][0];
-      console.log(point);
-
       // a bit of centering map 
-
       angular.extend($scope, {
           center: {
             lat: point[1] + 0.04,
