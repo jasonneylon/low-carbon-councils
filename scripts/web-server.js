f8d06b74a7012f71f15d9237bc3fe09cb6e6bf83
Jason Neylon <jason.neylon@gmail.com>
2014-09-13 12:55:30 +0100

    Added a web app

--- scripts/web-server.js
+++ scripts/web-server.js
@@ -1,8 +1,8 @@
 var express = require("express");
-var logfmt = require("logfmt");
+// var logfmt = require("logfmt");
 var app = express();
 
-app.use(logfmt.requestLogger());
+// app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
   res.send('<h1>Low carbon councils</h1>');
