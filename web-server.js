53dbcce05939ba81a55a5637c00b14b22bb725ad
Jason Neylon <jason.neylon@gmail.com>
2014-09-13 13:20:55 +0100

    Finally basic angular app up and running

--- web-server.js
+++ web-server.js
@@ -1,12 +1,15 @@
-var express = require("express");
+var express = require("express"),
+  path = require("path");
 // var logfmt = require("logfmt");
 var app = express();
 
 // app.use(logfmt.requestLogger());
 
-app.get('/', function(req, res) {
-  res.send('<h1>Low carbon councils</h1>');
-});
+app.use(express.static(__dirname + '/app'));
+
+// app.get('/', function(req, res) {
+//   res.send('<h1>Low carbon councils</h1>');
+// });
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
